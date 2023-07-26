4c23f92e74c16b7372c3edbbf8a432a89862d951
owen mei <owenzackmei@gmail.com>
2023-07-26 15:31:05 -0400

    init commit, exercises done

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+var age;
+
 // Exercise 2
 
+const firstName = "Owen";
+lastName = "Mei";
+age = 17;
+console.log(`hey, my name is ${firstName} ${lastName}, and I am ${age} years old.`);
+
 // Exercise 3
+
+let language = "javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} is a programming language created in the year ${createdYear}, and it is ${isCaseSensitive} that it is case sensitive.`);
+
+// Exercise 4
+
+let price = 19.99; 
+let isOnSale = false;
+let salePercentage = 15; 
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(`The price of this item is $${price}, it is ${isOnSale} that it 
+is on sale, the sale percentage is ${salePercentage}, there are currently ${stock} 
+in our stock so it is ${inStock} that it is in stock, and the size is ${selectedSize}.`);
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`the title of this book is ${title}, by {author}. it has ${pageCount} and you are currently on page ${bookmark}. have you read this book yet? ${hasRead}.`);
